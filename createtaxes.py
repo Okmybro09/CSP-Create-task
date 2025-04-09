4dd589449e5772d0071f0b82818b9632a7f49fd0
Okaymybro09 <ashtonsimpson2009@gmail.com>
2025-04-09 14:48:21 -0500

    added lists not dicts and made the indexing "work"

--- createtaxes.py
+++ createtaxes.py
@@ -1,57 +1,110 @@
-#greets user for explanation 
 
-statesalestax = {
-    "Alabama": 4.00,
-    "Alaska": 0.00,
-    "Arizona": 5.60,
-    "Arkansas": 6.00,
-    "California": 6.00,
-    "Colorado": 2.90,
-    "Connecticut": 6.35,
-    "Delaware": 0.00,
-    "Florida": 6.00,
-    "Georgia": 4.00,
-    "Hawaii": 4.00,
-    "Idaho": 6.00,
-    "Illinois": 6.25,
-    "Indiana": 7.00,
-    "Iowa": 6.00,
-    "Kansas": 6.50,
-    "Kentucky": 6.00,
-    "Louisiana": 4.45,
-    "Maine": 5.50,
-    "Maryland": 6.00,
-    "Massachusetts": 6.25,
-    "Michigan": 6.00,
-    "Minnesota": 6.875,
-    "Mississippi": 7.00,
-    "Missouri": 4.225,
-    "Montana": 0.00,
-    "Nebraska": 5.50,
-    "Nevada": 6.85,
-    "New Hampshire": 0.00,
-    "New Jersey": 6.625,
-    "New Mexico": 5.125,
-    "New York": 4.00,
-    "North Carolina": 4.75,
-    "North Dakota": 5.00,
-    "Ohio": 5.75,
-    "Oklahoma": 4.50,
-    "Oregon": 0.00,
-    "Pennsylvania": 6.00,
-    "Rhode Island": 7.00,
-    "South Carolina": 6.00,
-    "South Dakota": 4.50,
-    "Tennessee": 7.00,
-    "Texas": 6.25,
-    "Utah": 4.70,
-    "Vermont": 6.00,
-    "Virginia": 5.30,
-    "Washington": 6.50,
-    "West Virginia": 6.00,
-    "Wisconsin": 5.00,
-    "Wyoming": 4.00
-}
+statesalestax = [
+    4.00,
+    0.00,
+    5.60,
+    6.00,
+    6.00,
+    2.90,
+    6.35,
+    0.00,
+    6.00,
+    4.00,
+    4.00,
+    6.00,
+    6.25,
+    7.00,
+    6.00,
+    6.50,
+    6.00,
+    4.45,
+    5.50,
+    6.00,
+    6.25,
+    6.00,
+    6.875,
+    7.00,
+    4.225,
+    0.00,
+    5.50,
+    6.85,
+    0.00,
+    6.625,
+    5.125,
+    4.00,
+    4.75,
+    5.00,
+    5.75,
+    4.50,
+    0.00,
+    6.00,
+    7.00,
+    6.00,
+    4.50,
+    7.00,
+    6.25,
+    4.70,
+    6.00,
+    5.30,
+    6.50,
+    6.00,
+    5.00,
+    4.00
+]
+
+
+states = [
+    "Alabama -1",
+    "Alaska -2",
+    "Arizona -3",
+    "Arkansas -4",
+    "California -5",
+    "Colorado -6",
+    "Connecticut -7",
+    "Delaware -8",
+    "Florida -9",
+    "Georgia -10",
+    "Hawaii -11",
+    "Idaho -12",
+    "Illinois -13",
+    "Indiana -14",
+    "Iowa -15",
+    "Kansas -16",
+    "Kentucky -17",
+    "Louisiana -18",
+    "Maine -19",
+    "Maryland -20",
+    "Massachusetts -21",
+    "Michigan -22",
+    "Minnesota -23",
+    "Mississippi -24",
+    "Missouri -25",
+    "Montana -26",
+    "Nebraska -27",
+    "Nevada -28",
+    "New Hampshire -29",
+    "New Jersey -30",
+    "New Mexico -31",
+    "New York -32",
+    "North Carolina -33",
+    "North Dakota -34",
+    "Ohio -35",
+    "Oklahoma -36",
+    "Oregon -37",
+    "Pennsylvania -38",
+    "Rhode Island -39",
+    "South Carolina -40",
+    "South Dakota -41",
+    "Tennessee -42",
+    "Texas -43",
+    "Utah -44",
+    "Vermont -45",
+    "Virginia -46",
+    "Washington -47",
+    "West Virginia -48",
+    "Wisconsin -59",
+    "Wyoming -50"
+]
 
 
 def datagather():
@@ -66,18 +119,24 @@ def datagather():
 
         print("hello what state are you in?")
         print("the options are")
-        for key in statesalestax.keys():
-             print(key)
-
-
-    stateinput = input("input state ")
-    return stateinput
-
+        for item in states:
+             print(item)
+        
+        stateinput = input("input state ")  
+    
+        for i, state in enumerate(states):
+            if stateinput.lower() in state.lower():
+                return i
+        stateinput = input("input state ")
+        return stateinput
+    
 def taxcalc(state):
-    statetax = statesalestax[state]
-    pretax = float(input("what is the cost of the transaction $"))
-    tax = pretax * (statetax / 100)
-    print(f"The tax on a {pretax} purchase in {state} is ${tax}")
+    if state is not None:  
+        statetax = statesalestax[state]
+        pretax = float(input("what is the cost of the transaction $"))
+        tax = pretax * (statetax / 100)
+        state_name = states[state].split(" -")[0] 
+        print(f"The tax on a ${pretax} purchase in {state_name} is ${tax}")
 
 
 
